5c71da3fd460ced282119467d72d25a9c1184a23
Kelsey O'Hagan <kelsey@rascality.nz>
2021-03-12 14:58:04 +1300

    Create open hours js script to update the hours dynamically.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ https://cdn.jsdelivr.net/gh/user/repo@version/file
 
 i.e.
 https://cdn.jsdelivr.net/gh/Rascality/hb-wf@COMMIT_NUMBER/base.js
+https://cdn.jsdelivr.net/gh/Rascality/hb-wf@COMMIT_NUMBER/open-hours.js
---
+++ open-hours.js
@@ -0,0 +1,44 @@
+var openHours = [];
+$('.open-hour').each(function() {
+  var openHour = {
+    day: $(this).children('.open-hour-day').first().text(),
+    open: $(this).children('.open-hour-open').first().text(),
+    close: $(this).children('.open-hour-close').first().text(),
+    closed: $(this).children('.open-hour-closed.w-condition-invisible').length == 0
+  };
+  openHours.push(openHour);
+});
+
+var DateTime = luxon.DateTime;
+var message = '';
+var currentHours = null;
+
+if (DateTime) {
+  const weekday = DateTime.now().setZone('Australia/Melbourne').weekdayLong;
+  for (var i = 0; i < openHours.length; i++) {
+    var hour = openHours[i];
+    if (weekday === hour['day']) {
+      currentHours = hour;
+    }
+  }
+
+  if (currentHours) {
+    if (currentHours['closed']) {
+      message = 'Closed Today';
+    } else {
+      try {
+        var open = DateTime.fromJSDate(new Date(currentHours['open']));
+        var openHour = open.toLocaleString({ hour: 'numeric', minute: '2-digit', hour12: true }).replace(':00 ', '');
+
+        var close = DateTime.fromJSDate(new Date(currentHours['close']));
+        var closeHour = close.toLocaleString({ hour: 'numeric', minute: '2-digit', hour12: true }).replace(':00 ', '');
+
+        message = `${openHour}–${closeHour} Today`;
+      } catch (err) {
+        console.log('Error parsing date', err);
+      }
+    }
+  }
+}
+
+$('.open-hour-status').text(message);
